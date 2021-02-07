f1f81d372132b2997c15452d7e70765bd613a5e3
豪宗超 <haozch@minstone.com.cn>
2021-02-07 11:44:38 +0800

    初始化vdoingBlog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  // theme: 'vdoing', // 使用依赖包主题
+  theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "HoweBlog",
+  description: '在技术道路上造BUG的菜鸟',
+  base: '/myBlog/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
